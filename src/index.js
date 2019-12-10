ea56bfa7583c31bacfb86e12d85b1721bdd9f31f
Jordle <JCWardle@users.noreply.github.com>
2019-12-11 10:24:18 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
-var tbc_lib_std = require('std/index.js');
+const std = require('std/index.js');
 var tbc_lib_array = require('array/index.js');
 var tbc_lib_color = require('color/index.js');
 var tbc_lib_formatting = require('formatting/index.js');
 var tbc_lib_math = require('math/index.js');
 var tbc_lib_date = require('date/index.js');
 var tbc_lib_dom = require('dom/index.js');
+
+module.exports = {std}
